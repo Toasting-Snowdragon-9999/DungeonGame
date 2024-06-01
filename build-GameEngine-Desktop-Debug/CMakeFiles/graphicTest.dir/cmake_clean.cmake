269d79fe030a7857f3d408088fd4f6b7eacfbb3e
christopher <phfae23@student.sdu.dk>
2024-06-02 00:43:15 +0200

    textures

---
+++ build-GameEngine-Desktop-Debug/CMakeFiles/graphicTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphicTest.dir/main.cpp.o"
+  "CMakeFiles/graphicTest.dir/main.cpp.o.d"
+  "graphicTest"
+  "graphicTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphicTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-GameEngine-Desktop-Debug/CMakeFiles/graphicTest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for graphicTest.
+# This may be replaced when dependencies are built.
